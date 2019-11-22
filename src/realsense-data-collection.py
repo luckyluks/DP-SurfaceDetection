e9e940be83fb658af54786cc9ef71113a35418d9
luckyluks <lukas.rauh@r-a-u-h.de>
2019-11-22 11:35:52 +0100

    added space and time infos to recording file

--- src/realsense-data-collection.py
+++ src/realsense-data-collection.py
@@ -5,36 +5,13 @@ import numpy as np
 import sys
 import cv2
 import time
+import shutil
 
 # use own libs
 from utils import *
 
-# # Configure depth and color streams
-# pipeline = rs.pipeline()
-# config = rs.config()
-# config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 6)
-# config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 6)
-
-
-
-# if not os.path.exists(directory):
-# 	os.mkdir(directory)
-# try:
-# 	pipeline.start(config)
-# 	i = 0
-# 	while True:
-# 		print("Saving frame:", i)
-# 		frames = pipeline.wait_for_frames()
-# 		depth_frame = frames.get_depth_frame()
-# 		depth_image = np.asanyarray(depth_frame.get_data())
-# 		cv2.imwrite(directory + "/" + str(i).zfill(6) + ".png", depth_image)
-# 		i += 1
-# finally:
-# 	pass
-
-
 # test flag, if true no file will be dumped
-test_run = True
+test_run = False
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -50,26 +27,31 @@ profile = pipeline.start(config)
 sensor_depth = pipeline.get_active_profile().get_device().query_sensors()[0]
 sensor_rgb = pipeline.get_active_profile().get_device().query_sensors()[1]
 # reset to 100
-sensor_rgb.set_option(rs.option.exposure, 100.0)
-
-
-
+sensor_rgb.set_option(rs.option.exposure, 200.0)
+sensor_rgb.set_option(rs.option.white_balance, 2800.0)
 
 # Configure file settings
+total, used, free = shutil.disk_usage("/")
+
+print("Disk space: Total/Used/Free: {}/{}/{} GiB".format((total // (2**30)), (used // (2**30)), (free // (2**30))) )
 clip_length = 30 # in seconds
+warmup_length = 5 # in seconds
 
 codec = cv2.VideoWriter_fourcc(*'mp4v')
 directory = "recordings"
 
 
 if not test_run:
-    out = cv2.VideoWriter(os.path.join(directory,generate_save_date_name('.mp4', 'object_detection')), codec, framerate, (frame_width,frame_height))
-    out_depth = cv2.VideoWriter(os.path.join(directory,generate_save_date_name('.mp4', 'object_detection_depth')), codec, framerate, (frame_width,frame_height))
+    file_path_rgb = os.path.join(directory,generate_save_date_name('.mp4', 'object_detection_{}s'.format(clip_length) ))
+    file_path_depth = os.path.join(directory,generate_save_date_name('.mp4', 'object_detection_depth_{}s'.format(clip_length) ))
+
+    out = cv2.VideoWriter(file_path_rgb, codec, framerate, (frame_width,frame_height))
+    out_depth = cv2.VideoWriter(file_path_depth, codec, framerate, (frame_width,frame_height))
 # config.enable_record_to_file(str(directory+'/'+'object_detection.bag'))
 
 e1 = cv2.getTickCount()
 
-print("Run warmup...")
+print("Run warmup ({}s)...".format(warmup_length))
 while True:
     sensor_rgb.set_option(rs.option.enable_auto_exposure, 1.0)
     sensor_rgb.set_option(rs.option.enable_auto_white_balance, 1.0)
@@ -97,7 +79,7 @@ frame_counter = 0
 
 
 try:
-    print("Start recording...")
+    print("Start",("test run ({}s)..." if test_run else "recording ({}s)...").format(clip_length))
     while True:
 
         # Wait for a coherent pair of frames: depth and color
@@ -138,21 +120,12 @@ try:
         e2 = cv2.getTickCount()
         t = (e2 - e1) / cv2.getTickFrequency()
         if t > clip_length: # change it to record what length of video you are interested in
-            print("Done "+ ("test run" if test_run else "recording") +  ("" if test_run else str("\"",frame_counter,"\" frames") )+"!")
+            print("Done "+ ("test run" if test_run else "recording ") +  ("" if test_run else "\"{}\" frames".format(frame_counter) )+"!")
             # time.sleep(0.5)
             break
 
         if frame_counter % (framerate*5) == 0 :
             print("current passed time:", t)
-            sensor_rgb.set_option(rs.option.enable_auto_exposure, 0)
-            exp = pipeline.get_active_profile().get_device().query_sensors()[1].get_option(rs.option.exposure) # Get exposure
-            # exp2 = color_frame.get_frame_metadata(pyrealsense2.pyrealsense2.frame_metadata_value.exposure_roi_top)
-            print(exp)
-            # sensor_rgb.set_option(rs.option.enable_auto_exposure, 1)
-
-            sensor_rgb = pipeline.get_active_profile().get_device().query_sensors()[1]
-            sensor_rgb.set_option(rs.option.enable_auto_exposure, 0)
-            sensor_rgb.set_option(rs.option.auto_exposure_priority, 0)
 		
         # if (e2 - e1)/cv2.getTickFrequency() >= framerate*clip_length:
         #     print("Done recording \"",(e2 - e1)/cv2.getTickFrequency(),"\" frames!")
@@ -163,6 +136,10 @@ finally:
     if not test_run:
         out.release()
         out_depth.release()
+        print("created rgb file: {} ({})".format(file_path_rgb, file_size(file_path_rgb)))
+        print("created depth file: {} ({})".format(file_path_depth, file_size(file_path_depth)))
+
     # Stop streaming
     pipeline.stop()
+
 
