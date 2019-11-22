53356333635a580b3953bac6986a5c4f26250653
luckyluks <lukas.rauh@r-a-u-h.de>
2019-11-22 11:36:59 +0100

    added byte conversion to utils

--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,29 @@ def generate_save_date_name(file_ending, prefix="", suffix=""):
     time_str = str(time.strftime("%Y%m%d-%H%M%S"))
     full_suffix = ( prefix+"_" if prefix!="" else "") + time_str + ( "_"+suffix if suffix!="" else "") + file_ending
     return full_suffix
+
+def convert_bytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+
+def file_size(file_path):
+    """
+    this function will return the file size
+    """
+    if os.path.isfile(file_path):
+        file_info = os.stat(file_path)
+        return convert_bytes(file_info.st_size)
+
+
+def main():
+    print(os.path.join("directory",generate_save_date_name('.mp4', 'object_detection_30s')))
+
+
+if __name__ == '__main__':
+    main()
