977e9a07ca2de7ce0d323dbbdf43370a5727f48c
luckyluks <lukas.rauh@r-a-u-h.de>
2019-11-22 10:47:42 +0100

    added test running flag exposure warmup and fixing to the recording script

--- src/realsense-data-collection.py
+++ src/realsense-data-collection.py
@@ -6,6 +6,9 @@ import sys
 import cv2
 import time
 
+# use own libs
+from utils import *
+
 # # Configure depth and color streams
 # pipeline = rs.pipeline()
 # config = rs.config()
@@ -30,6 +33,9 @@ import time
 # 	pass
 
 
+# test flag, if true no file will be dumped
+test_run = True
+
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
@@ -39,22 +45,56 @@ frame_height = 480
 config.enable_stream(rs.stream.depth, frame_width, frame_height, rs.format.z16, framerate)
 config.enable_stream(rs.stream.color, frame_width, frame_height, rs.format.bgr8, framerate)
 
+# configure camera settings
+profile = pipeline.start(config)
+sensor_depth = pipeline.get_active_profile().get_device().query_sensors()[0]
+sensor_rgb = pipeline.get_active_profile().get_device().query_sensors()[1]
+# reset to 100
+sensor_rgb.set_option(rs.option.exposure, 100.0)
+
+
+
+
 # Configure file settings
 clip_length = 30 # in seconds
 
 codec = cv2.VideoWriter_fourcc(*'mp4v')
 directory = "recordings"
-config.enable_record_to_file(str(directory+'/'+'object_detection.bag'))
 
-out = cv2.VideoWriter(os.path.join(directory,'object_detection.mp4'), codec, framerate, (frame_width,frame_height))
-out_depth = cv2.VideoWriter(os.path.join(directory,'object_detection_depth.mp4'), codec, framerate, (frame_width,frame_height))
 
-# Start streaming
-pipeline.start(config)
+if not test_run:
+    out = cv2.VideoWriter(os.path.join(directory,generate_save_date_name('.mp4', 'object_detection')), codec, framerate, (frame_width,frame_height))
+    out_depth = cv2.VideoWriter(os.path.join(directory,generate_save_date_name('.mp4', 'object_detection_depth')), codec, framerate, (frame_width,frame_height))
+# config.enable_record_to_file(str(directory+'/'+'object_detection.bag'))
+
+e1 = cv2.getTickCount()
+
+print("Run warmup...")
+while True:
+    sensor_rgb.set_option(rs.option.enable_auto_exposure, 1.0)
+    sensor_rgb.set_option(rs.option.enable_auto_white_balance, 1.0)
+    frames = pipeline.wait_for_frames()
+
+    cv2.namedWindow('warmup', cv2.WINDOW_AUTOSIZE)
+    color_image = np.asanyarray(frames.get_color_frame().get_data())
+    cv2.imshow('warmup', color_image)
+    cv2.waitKey(1)
+
+    e2 = cv2.getTickCount()
+    t = (e2 - e1) / cv2.getTickFrequency()
+    if t > 5: # change it to record what length of video you are interested in
+        sensor_rgb.set_option(rs.option.enable_auto_exposure, 0.0)
+        sensor_rgb.set_option(rs.option.enable_auto_white_balance, 0.0)
+        print("Done warmup!")
+        break
+
+
 
 e1 = cv2.getTickCount()
 # print("current tick count:", cv2.getTickFrequency())
 frame_counter = 0
+
+
 
 try:
     print("Start recording...")
@@ -67,7 +107,8 @@ try:
         if not depth_frame or not color_frame:
             continue
 
-        
+        # exp2 = color_frame.get_frame_metadata(rs.frame_metadata_value.exposure)
+        # print(exp2)
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
@@ -85,9 +126,9 @@ try:
         images = np.hstack((color_image, depth_colormap))
 
 		# save frame
-        out.write(color_image)
-        #print(type(depth_u8))
-        out_depth.write(depth_colormap)
+        if not test_run:
+            out.write(color_image)
+            out_depth.write(depth_colormap)
         frame_counter+=1
 
         # Show images
@@ -97,12 +138,21 @@ try:
         e2 = cv2.getTickCount()
         t = (e2 - e1) / cv2.getTickFrequency()
         if t > clip_length: # change it to record what length of video you are interested in
-            print("Done recording \"",frame_counter,"\" frames!")
-            time.sleep(0.5)
+            print("Done "+ ("test run" if test_run else "recording") +  ("" if test_run else str("\"",frame_counter,"\" frames") )+"!")
+            # time.sleep(0.5)
             break
 
-        if frame_counter % (framerate*5) == 0:
-           print("current recording time:", t)
+        if frame_counter % (framerate*5) == 0 :
+            print("current passed time:", t)
+            sensor_rgb.set_option(rs.option.enable_auto_exposure, 0)
+            exp = pipeline.get_active_profile().get_device().query_sensors()[1].get_option(rs.option.exposure) # Get exposure
+            # exp2 = color_frame.get_frame_metadata(pyrealsense2.pyrealsense2.frame_metadata_value.exposure_roi_top)
+            print(exp)
+            # sensor_rgb.set_option(rs.option.enable_auto_exposure, 1)
+
+            sensor_rgb = pipeline.get_active_profile().get_device().query_sensors()[1]
+            sensor_rgb.set_option(rs.option.enable_auto_exposure, 0)
+            sensor_rgb.set_option(rs.option.auto_exposure_priority, 0)
 		
         # if (e2 - e1)/cv2.getTickFrequency() >= framerate*clip_length:
         #     print("Done recording \"",(e2 - e1)/cv2.getTickFrequency(),"\" frames!")
@@ -110,8 +160,9 @@ try:
 
 finally:
 	# Stop recording
-    out.release()
-    out_depth.release()
+    if not test_run:
+        out.release()
+        out_depth.release()
     # Stop streaming
     pipeline.stop()
 
